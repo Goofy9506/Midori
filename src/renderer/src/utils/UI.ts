b55e86248f7f4b3be35d658584a05955328acaa7
Goofy9506 <goofy9605@protonmail.com>
2024-08-08 16:26:32 -0500

    refactor: :lipstick: added theming to video player

--- src/renderer/src/utils/UI.ts
+++ src/renderer/src/utils/UI.ts
@@ -1,5 +1,8 @@
 export const getColorTheme = (theme: string) => {
   document.querySelector('#app')?.classList.remove('green')
   document.querySelector('#app')?.classList.remove('blue')
+  document.querySelector('#app')?.classList.remove('purple')
+  document.querySelector('#app')?.classList.remove('pink')
+  document.querySelector('#app')?.classList.remove('red')
   document.querySelector('#app')?.classList.add(theme)
 }
